283ad2ec82a215ff6b6a20e9205fc85aea6635cc
Harrison Pretel <harrisonp@MAC12131.local>
2020-10-25 09:18:08 -0500

    classes, events + more models

--- api/models.py
+++ api/models.py
@@ -5,9 +5,64 @@ import datetime
 # Create your models here.
 
 
+class School(models.Model):
+    name = models.CharField(max_length=25)
+    extension = models.CharField(max_length=25)
+    date_joined = models.DateTimeField()
+
+    def __str__(self):
+        return self.extension
+
+    def save(self, *args, **kwargs):
+        self.date_joined = datetime.datetime.now()
+        super(School, self).save(*args, **kwargs)
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=25)
+    school = models.ForeignKey(
+        School, related_name='departments', on_delete=models.CASCADE)
+    code = models.CharField(max_length=5)
+    date_joined = models.DateTimeField()
+
+    def __str__(self):
+        return self.name + ' at ' + self.school.name
+
+    def save(self, *args, **kwargs):
+        self.date_joined = datetime.datetime.now()
+        super(Department, self).save(*args, **kwargs)
+
+
+class Class(models.Model):
+    number = models.CharField(max_length=3)
+    department = models.ForeignKey(
+        Department, related_name='classes', on_delete=models.CASCADE
+    )
+    date_joined = models.DateTimeField()
+
+    def __str__(self):
+        return self.department.code + '' + self.number
+
+    def save(self, *args, **kwargs):
+        self.date_joined = datetime.datetime.now()
+        super(Class, self).save(*args, **kwargs)
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(
+        User, related_name='profile', on_delete=models.CASCADE)
+    classes = models.ManyToManyField(Class, related_name='students')
+    departments = models.ManyToManyField(Department, related_name='students')
+    schools = models.ManyToManyField(School, related_name='students')
+
+    def __str__(self):
+        return self.user.username
+
+
 class Message(models.Model):
     sender = models.ForeignKey(
-        User, related_name='sent_message', on_delete=models.CASCADE)
+        User, related_name='sent_message', on_delete=models.CASCADE
+    )
     receivers = models.ManyToManyField(User, related_name='received_message')
     message_type = models.CharField(max_length=10)
     content = models.CharField(max_length=140)
@@ -19,3 +74,30 @@ class Message(models.Model):
     def save(self, *args, **kwargs):
         self.timestamp = datetime.datetime.now()
         super(Message, self).save(*args, **kwargs)
+
+
+class Event(models.Model):
+    class_focus = models.ForeignKey(
+        Class, related_name='events', on_delete=models.CASCADE)
+    organizer = models.ForeignKey(
+        User, related_name='organized_events', on_delete=models.CASCADE)
+    time_organized = models.DateTimeField()
+
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    title = models.CharField(max_length=50)
+    size_limit = models.IntegerField()
+    link = models.CharField(max_length=500)
+    description = models.CharField(max_length=500)
+    status = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        self.time_organized = datetime.datetime.now()
+        super(Event, self).save(*args, **kwargs)
+
+
+class Attendance(models.Model):
+    user = models.ForeignKey(
+        User, related_name='attendance', on_delete=models.CASCADE)
+    event = models.ForeignKey(
+        Event, related_name='attendance', on_delete=models.CASCADE)
